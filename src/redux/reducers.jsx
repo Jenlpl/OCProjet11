b222b69d6d2eb5751dbb8da7c9335ef33ecc5bab
Evgeniya Plumey <e.plumey@outlook.com>
2024-03-27 21:44:52 +0100

    header

---
+++ src/redux/reducers.jsx
@@ -0,0 +1,11 @@
+/*import { combineReducers } from '@reduxjs/toolkit';
+import reducer1 from './reducer1';
+import reducer2 from './reducer2';
+
+const rootReducer = combineReducers({
+  reducer1,
+  reducer2,
+  // add other reducers here
+});
+
+export default rootReducer;*/
