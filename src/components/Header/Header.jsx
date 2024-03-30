891f7739a8eb862009ffb4dc997b2d908e369921
Evgeniya Plumey <e.plumey@outlook.com>
2024-03-30 22:31:53 +0100

    pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import Logo from '../../images/argentBankLogo.png';
-
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../images/argentBankLogo.webp';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   return (
-    <>
-    <nav class="main-nav">
-      <a class="main-nav-logo" href="./index.html">
-        <img
-          class="main-nav-logo-image"
-          src={Logo}
-          alt="Argent Bank Logo"
-        />
-        <h1 class="sr-only">Argent Bank</h1>
-      </a>
-      <div>
-        <a class="main-nav-item" href="./sign-in.html">
-          <i class="fa fa-user-circle"></i>
-          Sign In
-        </a>
-      </div>
-    </nav>
-    </>
-  )
+    <header>
+      <nav className="main-nav">
+        <NavLink to="/" className="main-nav-logo">
+          <img
+            className="main-nav-logo-image"
+            src={Logo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </NavLink>
+        <div>
+          <NavLink to="/sign-in" className="main-nav-item">
+          <FontAwesomeIcon icon={faUserCircle} />
+            Sign In
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
 }
